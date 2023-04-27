1e21ca9a43cb3955b46773952c0ea407f7f1fb51
Brian Durand <bbdurand@gmail.com>
2023-04-26 19:08:58 -0700

    add indexes on zcta geoids

--- db/migrate/20230414000000_add_zcta_primary_place.rb
+++ db/migrate/20230414000000_add_zcta_primary_place.rb
@@ -4,10 +4,10 @@ class AddZctaPrimaryPlace < ActiveRecord::Migration[5.0]
   def up
     return if column_exists?(:us_geo_zctas, :primary_place_geoid)
 
-    add_column :us_geo_zctas, :primary_place_geoid, :string, limit: 7, null: true, index: true
+    add_column :us_geo_zctas, :primary_place_geoid, :string, limit: 7, null: true
   end
 
   def down
-    add_column :us_geo_zctas, :primary_place_geoid, :string, limit: 7, null: true, index: true
+    remove_column :us_geo_zctas, :primary_place_geoid
   end
 end
